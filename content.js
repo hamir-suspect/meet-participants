eb43d3f9b5f2249aae3cfcd2c1ed2ac8859da2f3
Mustafa Spahic <mustafa.spahic@fet.ba>
2020-04-05 16:58:04 +0200

    Added scroll
    Signed-off-by: hamir_suspect <amir.hasanbasic@fet.ba>

--- content.js
+++ content.js
@@ -1,19 +1,43 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    const re = 'cS7aqe NkoVdd';
-    var htmlImena = document.getElementsByClassName(re);
-    let imena=[];
-    console.log("ulazimo u peltju");
-    
-    for (i = 0; i < htmlImena.length; i++) {
-        console.log(htmlImena[i].innerHTML);
-        imena.push(htmlImena[i].innerHTML);
-      }
-      console.log("imena");
-     console.log(imena);
+  const re = "cS7aqe NkoVdd";
+  var htmlImena = document.getElementsByClassName(re);
+  let imena = [];
 
-     sendResponse({
-        url: window.location.href,
-        count: imena.length,
-        data: imena
-      })
-    })
+  for (i = 0; i < htmlImena.length; i++) {
+    imena.push(htmlImena[i].innerHTML);
+  }
+
+  let scrollBar = document.getElementsByClassName(
+    "TnISae CnDs7d hPqowe crOkHf"
+  )[0];
+
+  if (scrollBar) {
+    scrollBar.scrollIntoView({ block: "center" });
+    htmlImena = document.getElementsByClassName(re);
+
+    for (i = 0; i < htmlImena.length; i++) {
+      if (imena.includes(htmlImena[i].innerHTML)) {
+        console.log("postoji vec :" + htmlImena[i].innerHTML);
+        continue;
+      }
+      imena.push(htmlImena[i].innerHTML);
+    }
+
+    scrollBar.scrollIntoView({ block: "end" });
+    htmlImena = document.getElementsByClassName(re);
+
+    for (i = 0; i < htmlImena.length; i++) {
+      if (imena.includes(htmlImena[i].innerHTML)) {
+        console.log("postoji vec :" + htmlImena[i].innerHTML);
+        continue;
+      }
+      imena.push(htmlImena[i].innerHTML);
+    }
+  }
+
+  sendResponse({
+    url: window.location.href,
+    count: imena.length,
+    data: imena,
+  });
+});
